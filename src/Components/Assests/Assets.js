6654340cef59188a30860741893a105d07c621b3
muraliofficial <murali_official@outlook.com>
2024-11-22 15:17:38 +0530

    Header page

--- src/Components/Assests/Assets.js
+++ src/Components/Assests/Assets.js
@@ -1,7 +1,21 @@
-import testimg from "./testimg.png";
+import giftitem from './Header/Gifts.jpg'
+import Bangles from './Header/Bangles and Earings.jpg'
+import Handbags from './Header/Handbags.jpg'
+import Makeupitems from './Header/Makeup Items.jpg'
+import perfumes from './Header/Perfumes.jpg'
+import smallDecors from './Header/small decors.jpg'
+import stationery from './Header/Stationery.jpg'
+import toys from './Header/Toys.jpg'
 
 const assets = {
-    testimg
+    giftitem,
+    Bangles,
+    Handbags,
+    Makeupitems,
+    perfumes,
+    smallDecors,
+    stationery,
+    toys
 }
 
 export default assets;
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,16 +1,40 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './Header.css';
 import assets from '../Assests/Assets';
+import AOS from 'aos';
+import 'aos/dist/aos.css'; // Import AOS styles
 
 const Header = () => {
+  const images = [
+    { src: assets.Bangles, alt: 'Bangles' },
+    { src: assets.Handbags, alt: 'Handbags' },
+    { src: assets.Makeupitems, alt: 'Makeup Items' },
+    { src: assets.giftitem, alt: 'Gift Item' },
+    { src: assets.perfumes, alt: 'Perfumes' },
+    { src: assets.smallDecors, alt: 'Small Decors' },
+    { src: assets.stationery, alt: 'Stationery' },
+    { src: assets.toys, alt: 'Toys' },
+  ];
+
+  useEffect(() => {
+    AOS.init({ duration: 1000, once: true }); // Initialize AOS with custom settings
+  }, []);
+
   return (
-    <div className="body">
-      <div className="box">
-        {[...Array(8)].map((_, index) => (
-          <span style={{ '--i': index + 1 }} key={index}>
-            <img src={assets.testimg} alt={`Cube side ${index + 1}`} />
-          </span>
-        ))}
+    <div className="container-fluid">
+      <div className="head">
+        {/* Add any header content here if needed */}
+      </div>
+      <div className="body" data-aos="fade-up">
+        <h1>Welcome To Our Shop</h1>
+        <h3>Explore Our Newest</h3>
+        <div className="box">
+          {images.map((image, index) => (
+            <span key={index} style={{ '--i': index + 1 }}>
+              <img src={image.src} alt={image.alt} />
+            </span>
+          ))}
+        </div>
       </div>
     </div>
   );
